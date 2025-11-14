7da83b0a9b415857805bb0b099ce5646a5c9b1b3
pieterj2000 <19341833+pieterj2000@users.noreply.github.com>
2025-11-14 14:38:46 +0100

    downloader

--- download.jl
+++ download.jl
@@ -1,3 +1,65 @@
 
+import HTTP
+import JSON
+using RateLimiter
 
-3
+const key = read("key", String)
+
+function getPage(query :: String, page :: Integer )
+    spul = "https://xeno-canto.org/api/3/recordings?query="
+    spulen = spul * HTTP.escapeuri(query) * "&key=" * key * "&per_page=500&page=" * string(page)
+    #println(spulen)
+    r = HTTP.get(spulen)
+    b = JSON.parse(String(r.body))
+
+    numpages = b["numPages"]
+    recs = b["recordings"]
+        
+    if page == numpages
+        return recs
+    else
+        return [ recs ; getPage(query, page+1) ]
+    end
+end
+
+function getQuery(query :: String)
+    getPage(query, 1)
+end
+
+function makedownloadsfolder()
+    rm("data/downloads"; force=true, recursive=true)
+    mkpath("data/downloads")
+end
+
+
+function metaToFile(ding :: JSON.Object{String, Any})
+    id = ding["id"] # is opgeslagen als string
+    open("data/downloads/XC" * id, "w") do file
+        JSON.json(file, ding, pretty=true)
+    end
+end
+
+function metaToFile(spul :: Vector)
+    metaToFile.(spul)
+end
+
+limiter = TokenBucketRateLimiter(10,3,0)
+
+function download(ding :: JSON.Object{String, Any})
+    url = ding["file"]
+    r = @rate_limit limiter 1 HTTP.get(url)
+    filenamecont = HTTP.header(r, "content-disposition")
+    filenameparts = split(filenamecont, ['.', ' ', '\"'])
+    filename = filenameparts[3] * "." * filenameparts[end-1]
+    #if filenameparts[end-1] ∉ ["wav", "mp3"]
+    #    println("################################## " * filename)
+    #end
+    println(filename)
+    write("data/downloads/" * filename, HTTP.body(r))
+end
+
+dingen = getQuery("sp:\"parus major\" q:A")
+
+makedownloadsfolder()
+metaToFile(dingen)
+download.(dingen)
